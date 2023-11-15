1be60b0ca59f5f3690a2b0d36bdc3f30ffc7b186
jessiline <95264872+jessiline@users.noreply.github.com>
2023-11-15 22:04:45 +0700

    Add files via upload
    final

--- W9-MobComp/W9-MobComp/ContentView.swift
+++ W9-MobComp/W9-MobComp/ContentView.swift
@@ -1,43 +1,211 @@
 import SwiftUI
-
+struct LegalitiesPair: Hashable {
+    let value: String
+    let key: String
+}
 struct MTGCardView: View {
-    var card: MTGCard
+    var mtgCards: [MTGCard]
+    @State private var currentIndex: Int
+    @State private var selectedButton: String?
+    
+    init(mtgCards: [MTGCard], currentIndex: Int) {
+        self.mtgCards = mtgCards
+        self._currentIndex = State(initialValue: currentIndex)
+    }
     
     var body: some View {
         VStack {
-            // Tampilkan gambar kartu
-            AsyncImage(url: URL(string: card.image_uris?.large ?? "")) { phase in
-                switch phase {
-                case .success(let image):
-                    image
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                case .failure:
-                    Image(systemName: "exclamationmark.triangle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(.red)
-                case .empty:
-                    ProgressView()
-                @unknown default:
-                    ProgressView()
+            ScrollView {
+                AsyncImage(url: URL(string: mtgCards[currentIndex].image_uris?.large ?? "")) { phase in
+                    switch phase {
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    case .failure:
+                        Image(systemName: "exclamationmark.triangle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .foregroundColor(.red)
+                    case .empty:
+                        ProgressView()
+                    @unknown default:
+                        ProgressView()
+                    }
+                    // judul yg di detail card
+                    Text(mtgCards[currentIndex].name)
+                        .font(.title)
+                        .padding()
+                    VStack(alignment: .leading) {
+                        Text("\(mtgCards[currentIndex].type_line)") //deskripsi kartu
+                            .fontWeight(.bold)
+                        Text("(\(mtgCards[currentIndex].oracle_text))")
+                        HStack {
+                            Spacer()
+                            
+                            // Button u/ Versions
+                            Button("Versions") {
+                                selectedButton = "Versions"
+                            }
+                            .padding(.horizontal, 45.0)
+                            .padding(.vertical, 15.0)
+                            .frame(maxHeight: .infinity)
+                            .background(
+                                Capsule()
+                                .fill(selectedButton == "Versions" ? Color.red : Color.clear) // Change background color to red if selected
+                                .overlay(
+                                    Capsule()
+                                        .stroke(Color.gray, lineWidth: 1)
+                                )
+                            )
+                            .foregroundColor(selectedButton == "Versions" ? .white : .black)
+                            
+                            
+                            // Button u/ Rulings
+                            Button("Rulings") {
+                                selectedButton = "Rulings"
+                            }
+                            .padding(.horizontal, 50.0)
+                            .frame(maxHeight: .infinity)
+                            .background(
+                                Capsule()
+                                    .fill(selectedButton == "Rulings" ? Color.red : Color.clear) // Change background color to red if selected
+                                    .overlay(
+                                        Capsule()
+                                            .stroke(Color.gray, lineWidth: 1)
+                                    )
+                            )
+                            .foregroundColor(selectedButton == "Rulings" ? .white : .black)
+                            
+                            Spacer()
+                        }
+                        .padding(.vertical, 16)
+                        
+                        // cek yg di klik rulings/version
+                        if selectedButton == "Rulings" {
+                            Text("LEGALITIES")
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.red)
+                            
+                            LazyVGrid(columns: [GridItem(), GridItem()]) {
+                                ForEach(Array(Mirror(reflecting: mtgCards[currentIndex].legalities).children), id: \.label) { child in
+                                    if let label = child.label, let value = child.value as? String {
+                                        let displayValue = value == "not_legal" ? "not legal" : value
+                                        LegalitiesItem(value: displayValue, key: label.capitalized)
+                                    }
+                                }
+                            }
+                            .padding()
+                        }else if selectedButton == "Versions" {
+                            Text("PRICES")
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.red)
+                            
+                            ForEach(Array(Mirror(reflecting: mtgCards[currentIndex].prices ?? [:]).children), id: \.label) { child in
+                                if let label = child.label, let value = child.value as? String {
+                                    let formattedKey = label.replacingOccurrences(of: "_", with: " ").capitalized
+                                    PriceItem(value: value, key: formattedKey)
+                                }
+                            }
+                            
+                            .padding()
+                        }
+                    }
+                    HStack {
+                        Button("Previous") {
+                            navigateToPreviousCard()
+                        }
+                        .padding()
+                        .disabled(currentIndex == 0) // Disable the button if it's the first card
+                        
+                        Spacer()
+                        
+                        Button("Next") {
+                            navigateToNextCard()
+                        }
+                        .padding()
+                        .disabled(currentIndex == mtgCards.count - 1) // Disable the button if it's the last card
+                    }
                 }
-            }
-            
-            // Tampilkan nama kartu
-            Text(card.name)
-                .font(.title)
                 .padding()
-            
-            // Tampilkan jenis kartu dan teks orakel
-            VStack(alignment: .leading) {
-                Text("Type: \(card.type_line)")
-                Text("Oracle Text: \(card.oracle_text)")
+                
             }
-            .padding()
+            .gesture(
+                DragGesture()
+                    .onEnded { gesture in
+                        let swipeThreshold: CGFloat = 50
+                        if gesture.translation.width > swipeThreshold {
+                            navigateToPreviousCard()
+                        } else if gesture.translation.width < -swipeThreshold {
+                            navigateToNextCard()
+                        }
+                    }
+            )
+        }
+        
+
+    }
+    private func navigateToNextCard() {
+        if currentIndex < mtgCards.count - 1 {
+            currentIndex += 1
+        }
+        selectedButton = nil
+    }
+    
+    // Function to navigate to the previous card
+    private func navigateToPreviousCard() {
+        if currentIndex > 0 {
+            currentIndex -= 1
+        }
+        selectedButton = nil
+    }
+}
+
+
+extension MTGCardView: Identifiable {
+    var id: UUID { mtgCards[currentIndex].id }
+}
+
+struct PriceItem: View {
+    var value: String
+    var key: String
+
+    var body: some View {
+        HStack {
+            Text("\(key):")
+            Text(value)
+                .fontWeight(.bold)
         }
     }
 }
+
+struct LegalitiesItem: View {
+    var value: String
+    var key: String
+    
+    var body: some View {
+        HStack {
+            Text(value)
+                .foregroundColor(.white)
+                .padding(.vertical, 10)
+                .frame(width: 70)
+                .background(value == "legal" ? Color.green : Color.gray)
+                .cornerRadius(8)
+                .font(.system(size: 14))
+                .padding(.vertical, 10)
+
+            
+            Text(key)
+                .padding(.trailing, 5.0)
+                .frame(width: 90)
+                .foregroundColor(.black)
+                .font(.system(size: 14))
+            
+        }
+      
+    }
+}
+
 struct SearchBar: View {
     @Binding var searchText: String
 
@@ -69,6 +237,7 @@ struct SearchBar: View {
 struct ContentView: View {
     @State private var mtgCards: [MTGCard] = []
     @State private var searchText: String = ""
+    @State private var isSortingAscending = true
 
     let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
 
@@ -79,21 +248,38 @@ struct ContentView: View {
             return mtgCards.filter { $0.name.lowercased().contains(searchText.lowercased()) }
         }
     }
+    var sortedCards: [MTGCard] {
+        return filteredCards.sorted {
+            isSortingAscending ? $0.name < $1.name : $0.name > $1.name
+        }
+    }
 
     var body: some View {
         TabView {
             NavigationView {
                 ScrollView {
-                    SearchBar(searchText: $searchText)
+                    HStack {
+                       SearchBar(searchText: $searchText)
+                       
+                       // sorting button
+                       Button(action: {
+                           isSortingAscending.toggle()
+                       }) {
+                           Image(systemName: isSortingAscending ? "arrow.down.circle.fill" : "arrow.up.circle.fill")
+                               .foregroundColor(.blue)
+                       }
+                       .padding(.trailing, 10)
+                   }
 
                     LazyVGrid(columns: columns, spacing: 16) {
-                        ForEach(filteredCards) { card in
-                            NavigationLink(destination: MTGCardView(card: card)) {
-                                CardImageView(card: card)
+                        ForEach(sortedCards.indices, id: \.self) { index in
+                            NavigationLink(destination: MTGCardView(mtgCards: sortedCards, currentIndex: index)) {
+                                CardImageView(card: sortedCards[index])
                                     .frame(height: 220)
                             }
                         }
                     }
+
                     .padding()
                 }
                 .onAppear {
@@ -182,8 +368,8 @@ struct CardImageView: View {
             }
             
             Text(card.name)
-                .font(.system(size: 12)) // Adjust the size (e.g., 14) to your preference
-                .padding(.top, 8) // Adjust the padding as needed
+                .font(.system(size: 12))
+                .padding(.top, 8)
 
         }
     }
--- W9-MobComp/W9-MobComp/MTGCard.swift
+++ W9-MobComp/W9-MobComp/MTGCard.swift
@@ -7,23 +7,65 @@
 
 // MTGCard.swift
 import Foundation
-
+struct LegalitiesList: Codable {
+    var standard: String
+    var future: String
+    var historic: String
+    var gladiator: String
+    var pioneer: String
+    var explorer: String
+    var modern: String
+    var legacy: String
+    var pauper: String
+    var vintage: String
+    var penny: String
+    var commander: String
+    var oathbreaker: String
+    var brawl: String
+    var historicbrawl: String
+    var alchemy: String
+    var paupercommander: String
+    var duel: String
+    var oldschool: String
+    var premodern: String
+    var predh: String
+}
 struct MTGCard: Codable, Identifiable {
     var id: UUID
     var name: String
     var type_line: String
     var oracle_text: String
     var image_uris: ImageURIs?
-    
- 
-    // Define other properties as needed based on your JSON structure
+    var legalities: LegalitiesList
+    var prices: PricesList?
 
     struct ImageURIs: Codable {
         var small: String?
         var normal: String?
         var large: String?
-        // Add other image URL properties if needed
+
     }
+    struct PricesList: Codable {
+            var usd: String?
+            var usd_foil: String?
+            var usd_etched: String?
+            var eur: String?
+            var eur_foil: String?
+            var tix: String?
+        func dictionaryRepresentation() -> [String: String] {
+                    var dictionary = [String: String]()
+
+                    if let usd = usd { dictionary["USD"] = usd }
+                    if let usd_foil = usd_foil { dictionary["USD Foil"] = usd_foil }
+                    if let usd_etched = usd_etched { dictionary["USD Etched"] = usd_etched }
+                    if let eur = eur { dictionary["EUR"] = eur }
+                    if let eur_foil = eur_foil { dictionary["EUR Foil"] = eur_foil }
+                    if let tix = tix { dictionary["TIX"] = tix }
+
+                    return dictionary
+                }
+        }
+    
 }
 
 struct MTGCardList: Codable {
@@ -32,21 +74,4 @@ struct MTGCardList: Codable {
     var has_more: Bool
     var data: [MTGCard]
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
